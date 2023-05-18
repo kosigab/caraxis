5d639d7d88653351cca2e2240ddd3639ce6b5fbe
kosigab <kosigab@gmail.com>
2023-05-18 10:04:06 +0100

    Slide up panel now done correctly, maps control organic with display

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_tutorials/components/bottom_nav.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class DashboardPage extends StatefulWidget {
   @override
@@ -24,7 +25,8 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(Duration(seconds: 10), (Timer t) => _updateMarker());
+    _timer =
+        Timer.periodic(Duration(seconds: 10), (Timer t) => _updateMarker());
   }
 
   @override
@@ -51,8 +53,7 @@ class _DashboardPageState extends State<DashboardPage> {
         CameraPosition(
             target: LatLng(new_location[0], new_location[1]),
             zoom: 17.0,
-            tilt: 0
-        ),
+            tilt: 0),
       ),
     );
   }
@@ -66,58 +67,70 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard', style: GoogleFonts.roboto()),
-        ),
-        body: Stack(
-          children: [
-            GoogleMap(
-              onMapCreated: _onMapCreated,
-              markers: _markers,
-              initialCameraPosition: CameraPosition(
-                target: LatLng(new_location[0], new_location[1]),
-                zoom: 16.0,
+      appBar: AppBar(
+        title: Text('Dashboard', style: GoogleFonts.roboto()),
+      ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            markers: _markers,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(new_location[0], new_location[1]),
+              zoom: 16.0,
+            ),
+            zoomControlsEnabled: true,
+            // Enable zoom controls
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 1/25 + 10), // Push controls up
+          ),
+          SlidingUpPanel(
+            minHeight: MediaQuery.of(context).size.height * 1/25,
+            maxHeight: MediaQuery.of(context).size.height * 2 / 3,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16.0),
+              topRight: Radius.circular(16.0),
+            ),
+            panel: Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 100,
+                    color: index % 2 == 0 ? Colors.grey[100] : Colors.white10,
+                  );
+                },
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: FractionallySizedBox(
-                heightFactor: 0.33,
-                alignment: Alignment.bottomCenter,
-                child: Card(
-                  margin: EdgeInsets.all(0), // Make the card flush with screen edges
-                  color: Colors.white10,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16.0),
-                      topRight: Radius.circular(16.0),
+            collapsed: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0),
+                ),
+              ),
+              child: Center(
+                child: Container(
+                  width: 60,
+                  height: 5,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12.0),
                     ),
                   ),
-                  child: Container(
-                    // Your container code here
-                  ),
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 26.0),
-                child: FloatingActionButton(
-                  onPressed: () {
-                    // Implement SOS button functionality
-                  },
-                  child: Icon(Icons.warning_amber_outlined, size: 46),
-                  backgroundColor: Colors.red,
-                ),
-              ),
-            ),
-          ],
-        ),
-        bottomNavigationBar: BottomNav(
-          onItemTapped: _onItemTapped,
-          selectedIndex: _selectedIndex,
-        )
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNav(
+        onItemTapped: _onItemTapped,
+        selectedIndex: _selectedIndex,
+      ),
     );
   }
 }
